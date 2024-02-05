421058c7622795bc59f203b15ec211215823c86d
asing44 <adamwarsing@gmail.com>
2024-02-04 22:05:54 -0500

    Build test

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Capstone Blog',
   description: 'DMD 400 Capstone project',
   keywords: ['eleventy', 'template', 'simple', 'clean'],
-  url: 'https://vredeburg.netlify.app', // your site url without trailing slash
+  url: 'https://asing44.github.io/DMD-400-Blog/', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
